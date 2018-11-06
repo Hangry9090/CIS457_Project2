9b3c523ec988b2a22404a52d7860849f7f87f8a8
GionataB <gionatabonazzi@gmail.com>
2018-11-06 17:54:04 -0500

    Started + TODO

--- CentralizedServer.java
+++ CentralizedServer.java
@@ -1,5 +1,38 @@
-import java.util.ArrayList;
+import java.util.Vector;
 public class CentralizedServer{
-  private ArrayList<UserElement> userList;
-  private ArrayList<FileElement> fileList;
+  //Use Vectors to have a synchronized list. This way, multiple threads won't be able to change the list at the same time.
+  private Vector userList;
+  private Vector fileList;
+
+
+  private void getRequest(){
+
+    /*
+      TODO: accept the user and the xml file
+      TODO: create a UserElement with info from the user.
+    */
+
+    ArrayList<FileElement> files = parseData(file);
+    addUser(user);
+    addContent(files);
+  }
+
+  private ArrayList<FileElement> parseData(File file){
+    /*
+      TODO: parse the file, make a fileElement for each paragraph in the file.
+      TODO: in the end, put all the elements made in a list and return the list.
+    */
+  }
+
+  private void addUser(UserElement newUser){
+    synchronized(userList){
+      userList.addElement(newUser);
+    }
+  }
+
+  private void addContent(ArrayList<FileElement> newData){
+    synchronized(fileList){
+      fileList.addAll(newData);
+    }
+  }
 }
